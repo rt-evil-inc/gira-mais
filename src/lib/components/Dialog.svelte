d52ca51517ef4fa3880169aafe146463d96077da
ttmx <git@tteles.dev>
2025-12-30 13:36:22 +0000

    Fix debug padding on dialog

--- src/lib/components/Dialog.svelte
+++ src/lib/components/Dialog.svelte
@@ -7,7 +7,7 @@
 {#if toRender}
 	<div class="top-0 z-[100] fixed w-screen h-screen flex flex-col items-center justify-center transition-all"
 		transition:fade="{{ duration: 300, easing: cubicInOut }}"
-		style:padding-bottom={Math.max(10, keyboard.height - 20) + 'px'}>
+		style:padding-bottom={Math.max(0, keyboard.height - 20) + 'px'}>
 		<button class="absolute w-full h-full bg-black/40 -z-10"
 			onclick={() => toRender.dismiss() }
 			aria-label="Dismiss dialog"
