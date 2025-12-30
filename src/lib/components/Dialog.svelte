5d5d7f8e8ee326eff66cb0592fea0a61136ae69a
ttmx <git@tteles.dev>
2025-12-30 19:12:50 +0000

    fix scrolling on dialog

--- src/lib/components/Dialog.svelte
+++ src/lib/components/Dialog.svelte
@@ -3,15 +3,31 @@
 	import { cubicInOut } from 'svelte/easing';
 	import { fade } from 'svelte/transition';
 	let toRender = $derived(dialogQueue[0]);
+
+	function stopScrolling(e: TouchEvent) {
+		e.preventDefault();
+	}
+	$effect(() => {
+		if (toRender) {
+			document.documentElement.addEventListener('touchmove', stopScrolling, { passive: false });
+			console.log('--- added touchmove listener to stop scrolling');
+		} else {
+			document.documentElement.removeEventListener('touchmove', stopScrolling);
+			console.log('--- removed touchmove listener to stop scrolling');
+		}
+	});
 </script>
 {#if toRender}
 	<div class="top-0 z-[100] fixed w-screen h-screen flex flex-col items-center justify-center transition-all"
 		transition:fade="{{ duration: 300, easing: cubicInOut }}"
-		style:padding-bottom={Math.max(0, keyboard.height - 20) + 'px'}>
-		<button class="absolute w-full h-full bg-black/40 -z-10"
-			onclick={() => toRender.dismiss() }
-			aria-label="Dismiss dialog"
-		></button>
+	>
+		<div class="absolute w-full h-full bg-black/40 -z-10"
+			onpointerdown={() => {
+				toRender.dismiss();
+			}}
+			role="none"
+		></div>
 		{@render toRender.snippet(toRender.dismiss)}
+		<div style:padding-bottom={Math.max(0, keyboard.height - 20) + 'px'} class="w-full"></div>
 	</div>
 {/if}
